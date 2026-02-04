d56c2c18cd062b731d3cd528e3e7127f28ac0d1c
yousef-279 <y87646504@gmail.com>
2026-02-04 17:27:51 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,7 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://7w87tmi632.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
